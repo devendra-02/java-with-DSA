ff3b929b7fe4b225525f50118cc0beaaed3b06fd
Devendra <gautamdevendra12140@gmail.com>
2025-04-06 01:07:19 +0530

    small change

--- pwskill/src/Trees_/Node_class.java
+++ pwskill/src/Trees_/Node_class.java
@@ -11,8 +11,9 @@ class node{
 public class Node_class {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for(int i=0;i<10;i++) {
+			System.out.println("hellow dev");
+		}
 	}
 
 }
